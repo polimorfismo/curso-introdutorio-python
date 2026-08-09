be93db73b6e5420b3150e75bd15ebd371aa3a67d
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 11:55:51 -0300

    Create gerador_senhas_estrutura_inicial.py

---
+++ gerador_senhas_estrutura_inicial.py
@@ -0,0 +1,34 @@
+# 1. Importar o módulo random
+
+letras = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+simbolos = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+# 2. Mostre uma mensagem de boas vindas para o usuário
+
+# 3. Pergunte ao usuário quantas letras ele deseja que a senha possua e armazene em uma variável
+
+# 4. Pergunte ao usuário quantos símbolos ele deseja que a senha possua e armazene em uma variável
+
+# 5. Pergunte ao usuário quantos números ele deseja que a senha possua e armazene em uma variável
+
+# 6. Criar uma lista para armazenar todos os caracteres de senha gerados
+
+# 7. Criar um loop que percorra o intervalo entre 1 e o total de letras definido pelo usuário. A cada vez que o loop for executado sortear uma letra com o módulo random e armazená-la na lista.
+
+# 8. Criar um loop que percorra o intervalo entre 1 e o total de símbolos definido pelo usuário. A cada vez que o loop for executado sortear um símbolo com o módulo random e armazená-la na lista.
+
+# 9. Criar um loop que percorra o intervalo entre 1 e o total de números definido pelo usuário. A cada vez que o loop for executado sortear um número com o módulo random e armazená-la na lista.
+
+# 10. Embaralhar os caracteres armazenados na lista
+
+# 11. Criar uma variável para receber uma string concatenada
+
+# 12. Percorrer cada posição da lista e concatená-las. Os valores deverão ser salvos na variável criada no passo anterior.
+
+# 13. Imprimir a senha no console
