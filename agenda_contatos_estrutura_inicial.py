2d0dbef762c0e026bbbdc786918d9bb87b1a3a51
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 12:03:45 -0300

    Create agenda_contatos_estrutura_inicial.py

---
+++ agenda_contatos_estrutura_inicial.py
@@ -0,0 +1,17 @@
+# 1. Crie uma função que será responsável por exibir o menu do sistema com as opções: cadastro, pesquisa e remoção de cadastro e sair.
+
+# 2. Crie um loop while que será responsável por exibir as opções de sistema enquanto o usuário não escolher a opção '4. Sair'.
+
+# 3. Crie uma variável que ficará responsável por receber a opção de menu escolhida pelo usuário.
+
+# 4. Crie uma estrutura condicional para tornar o menu criado funcional. Utilize prints para simular as escolhas do menu.
+
+# 5. Faça o encerramento do programa se o usuário escolher a opção '4. Sair'.
+
+# 6. Criar um dicionário vazio que será responsável por armazenar os contatos.
+
+# 7. Crie uma função que será responsável por adicionar contatos ao dicionário criado no passo anterior. Campos para o contato: nome, telefone, email, endereço.
+
+# 8. Crie uma função que será responsável por consultar um contato. Caso este contato exista seus dados deverão sermostrados no console.
+
+# 9. Crie uma função responsável por remover um contato.
