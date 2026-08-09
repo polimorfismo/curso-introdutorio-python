1afd033033894e957e52025624c2d9f6940a753a
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 11:44:56 -0300

    Create projeto-gerador-nome-usuario-solucao.py

---
+++ projeto-gerador-nome-usuario-solucao.py
@@ -0,0 +1,15 @@
+# 1. Crie uma mensagem de boas vindas para o usuário
+
+print("Seja bem vindo ao gerador de nome de usuário!\n")
+
+# 2. Pergunte ao usuário qual é sua comida favorita
+
+comida_favorita = input("Qual é sua comida favorita? ")
+
+# 3. Pergunte ao usuário qual é o dia do seu aniversário
+
+dia_aniversario = input("Qual é o dia do seu aniversário? ")
+
+# 4. Faça a junção dos valores informados e mostre a sugestão de nome de usuário gerada
+
+print("\nO nome de usuário sugerido é: " + comida_favorita + dia_aniversario)
