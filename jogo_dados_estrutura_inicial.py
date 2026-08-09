b067130b6b37e5e04bcf6ab53e8f9894d3ceb604
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 11:54:21 -0300

    Create jogo_dados_estrutura_inicial.py

---
+++ jogo_dados_estrutura_inicial.py
@@ -0,0 +1,75 @@
+# 1. Faça a importação do módulo random no projeto
+
+# Faces dos dados
+dado_face1 = """
+ _______
+|       |
+|   ●   |
+|       |
+|_______|
+"""
+
+dado_face2 = """
+ _______
+| ●     |
+|       |
+|     ● |
+|_______|
+"""
+
+dado_face3 = """
+ _______
+| ●     |
+|   ●   |
+|     ● |
+|_______|
+"""
+
+dado_face4 = """
+ _______
+| ●   ● |
+|       |
+| ●   ● |
+|_______|
+"""
+
+dado_face5 = """
+ _______
+| ●   ● |
+|   ●   |
+| ●   ● |
+|_______|
+"""
+
+dado_face6 = """
+ _______
+| ●   ● |
+| ●   ● |
+| ●   ● |
+|_______|
+"""
+
+# 2. Crie uma lista que possui todas as faces do dado
+
+# 3. Mostre uma mensagem de boas vindas e um resumo de como o jogo funciona para o usuário
+
+# 4. Exiba uma mensagem solicitando que o usuário digite a letra J e pressione enter para jogar seus dados na mesa
+
+# 5. Gerar três números aleatórios para cada dado jogado pelo usuário na mesa e armazene-os em uma lista
+
+# 6. Exiba os valores obtidos para o usuário
+
+# 7. Exibir uma mensagem dizendo que o computador vai jogar os dados. Na sequência, gerar os números aleatórios para o computador, armazená-los em uma lista
+
+# 8. Mostrar os números obtidos para o computador
+
+# 9. Utilizar as listas e os índices para calcular o total de pontos de cada jogador através da soma dos valores obtidos nos três dados
+# Total de pontos usuário
+
+# Total de pontos computador
+
+# 10. Utilize condicionais para imprimir o resultado do jogo
+# Empate
+# Usuário vence
+# Computador vence
+
