5fada4d3b70f9df34f1c775550b0748366c46bb3
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 12:05:09 -0300

    Create calculadora_estrutura_inicial.py

---
+++ calculadora_estrutura_inicial.py
@@ -0,0 +1,13 @@
+# 1. Crie quatro funções que serão responsáveis por realizar as operações básicas de cálculo: soma, subtração, multiplicação e divisão. Cada uma dessas operações deverá receber 2 números como argumentos quando chamadas.
+
+# 2. Crie um dicionário chamado operacoes. Esse dicionário deverá fazer uma relação chave/valor de símbolos (+, -, * e /) com os nomes das funções que foram criadas.
+
+# 3. Utilize um input para receber o primeiro número do usuário.
+
+# 4. Crie um loop for para mostrar todas as operações disponíveis para o usuário e na sequência solicite que ele escolha uma das operações.
+
+# 5. Crie um input para receber o segundo número do usuário.
+
+# 6. Faça a chamada para a função de acordo com a operação escolhida pelo usuário. Utilize o 'valor' referente ao operador 'chave' do dicionário para chamar a função.
+
+# 7. Mostre o resultado da operação no console.
