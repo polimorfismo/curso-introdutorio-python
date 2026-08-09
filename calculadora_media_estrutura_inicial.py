2b26912870a72c2560157d28757e0113ab5ff95d
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 11:52:24 -0300

    Create calculadora_media_estrutura_inicial.py

---
+++ calculadora_media_estrutura_inicial.py
@@ -0,0 +1,15 @@
+# Projeto 3: Construa um programa que ajude um professor a calcular a média de três notas de um aluno. Caso o aluno entregue o trabalho extra, deverá ser adicionado 1 ponto em sua média. No final mostre a média do aluno e sua situação na disciplina, que poderá ser: aprovado, recuperação ou reprovado.
+
+# 1. Exibir mensagem de boas vindas
+
+# 2. Solicite que a pessoa digite as três notas do aluno
+
+# 3. Solicite que a pessoa informe se o aluno entregou ou não o trabalho extra
+
+# 4. Faça o cálculo da média e armazene em uma variável chamada 'media'
+
+# 5. Se o aluno entregou o trabalho extra adicione 1 ponto em sua media
+
+# 6. Faça impressão da média do aluno no console
+
+# 7. Crie uma estrutura condicional que mostre que o aluno foi aprovado se a media for maior ou igual a 7, recuperação se for 5 ou 6 e reprovado se for menor que 5
