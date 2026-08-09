c45cac6f086d06a2698543cd586b695a52ce1964
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 11:58:02 -0300

    Create adivinhe_numero_estrutura_inicial.py

---
+++ adivinhe_numero_estrutura_inicial.py
@@ -0,0 +1,7 @@
+# 1. importar o móduo random
+
+# 2. exibir uma mensagem de saudação para o jogador
+
+# 3. sortear um número aleatório entre 1 e 100
+
+# 4. crie um loop while que deverá perguntar um palpite de número para o jogador, até que ele acerte o número sorteado. Se o jogador acertar o número informe sobre o acerto e finalize o jogo. Se ele errar diga que o palpite está incorreto e também se ele está quente (quando o palpite estiver até cinco posições acima ou abaixo do número) ou frio (quando o palpite estiver mais de cinco posições acima ou abaixo do número).
