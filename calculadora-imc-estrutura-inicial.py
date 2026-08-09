c81e300826f354fe562b48edf8a7586fb1a12c82
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 11:46:12 -0300

    Create calculadora-imc-estrutura-inicial.py

---
+++ calculadora-imc-estrutura-inicial.py
@@ -0,0 +1,9 @@
+# 1. Crie uma mensagem de saudação para o usuário
+
+# 2. Peça para o usuário digitar o peso e armazene esse valor em uma variável
+
+# 3. Peça para o usuário digitar a altura e armazene esse valor em uma variável
+
+# 4. Realize o cálculo de IMC (lembre-se da sequência de prioridade na realização das operações)
+
+# 5. Mostre o resultado do IMC no console para o usuário (utilize f strings)
