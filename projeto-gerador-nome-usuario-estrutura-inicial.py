a00797420ce01ca68215f332092ffb8712fbbbce
Lucas Fagundes <51465884+lucasfv1@users.noreply.github.com>
2026-08-09 11:42:39 -0300

    Create projeto-gerador-nome-usuario-estrutura-inicial.py

---
+++ projeto-gerador-nome-usuario-estrutura-inicial.py
@@ -0,0 +1,7 @@
+# 1. Crie uma mensagem de boas vindas para o usuário
+
+# 2. Pergunte ao usuário qual é sua comida favorita
+
+# 3. Pergunte ao usuário qual é o dia do seu aniversário
+
+# 4. Faça a junção dos valores informados e mostre a sugestão de nome de usuário gerada no console
